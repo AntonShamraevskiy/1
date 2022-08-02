03c386a3bd49dd5c59fdd0b551737691af5e940d
AntonShamraevskiy <f1-84@mail.ru>
2022-08-02 22:10:45 +0300

    right

--- guess-number-task/game_v2.py
+++ guess-number-task/game_v2.py
@@ -5,22 +5,31 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
+def random_predict(number:int=np.random.randint(1, 101)) -> int:
     """Рандомно угадываем число
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Загаданное число. По умолчанию рандомно загадывается компьютером в диапазоне 1-100.
 
     Returns:
         int: Число попыток
     """
+
     count = 0
+    lst_num = list(range(101))
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        predict_number = int(np.mean(lst_num))
+        half = round((len(lst_num))/2)
         if number == predict_number:
-            break  # выход из цикла если угадали
+            break # выход из цикла, если угадали
+        elif predict_number < number:
+            lst_num = lst_num[half:]  
+        else:
+            lst_num = lst_num[:half]
+            
+        
     return count
 
 
